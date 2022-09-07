07205e4f4680e2152974aedfd92601f8d870fd73
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-09-07 14:59:44 +0530

    homepage sliders added

---
+++ src/Data.js
@@ -0,0 +1,7 @@
+export const banner = [
+    {image:"https://learnenglish.britishcouncil.org/sites/podcasts/files/styles/max_325x325/public/RS7940_ThinkstockPhotos-615720860-hig_0.jpg?itok=Kx-LkGXD"},
+    {image:"https://media.istockphoto.com/photos/young-woman-portrait-in-the-city-picture-id1009749608?k=20&m=1009749608&s=612x612&w=0&h=3bnVp0Y1625uKkSwnp7Uh2_y_prWbgRBH6a_6jRew3g="},
+    {image:"https://media.gettyimages.com/photos/confident-businesswoman-at-hotel-picture-id1192055634?b=1&k=20&m=1192055634&s=170667a&w=0&h=ZuJc9rdrr8cNrnPDydXk5wnlqwobWyeWtidwphcP67g="},
+    {image:"https://media.istockphoto.com/photos/portrait-of-handsome-latino-african-man-picture-id1007763808?k=20&m=1007763808&s=612x612&w=0&h=q4qlV-99EK1VHePL1-Xon4gpdpK7kz3631XK4Hgr1ls="},
+    {image:"https://media.istockphoto.com/photos/portrait-of-creative-trendy-black-african-male-designer-laughing-picture-id1296158947?k=20&m=1296158947&s=612x612&w=0&h=qh89sBTcLTUuuOZMMUwMrbfnWqERRyZch9R6Z74S9Lc="}
+]
--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./home.scss";
 import card1 from "../Images/card1-img.png";
 import abibbev from "../Images/abibbev.jpg";
@@ -11,8 +11,45 @@ import nivea from "../Images/nivea.png";
 import novo from "../Images/novo.png";
 import pernad from "../Images/pernad.png";
 import timesinternet from "../Images/timesinternet.jpg";
+import flag from "../Images/flag.png";
+
+import comma from "../Images/Svg/comma.svg";
+import slack from "../Images/Svg/slack.svg";
+import teams from "../Images/Svg/teams.svg";
+import { banner } from "../../Data";
 
 const Home = () => {
+
+  var timer;
+  const [slider, setSlider] = useState(0);
+ 
+  useEffect(()=>{
+    timer = setInterval(()=>{
+      setSlider(slider+1)
+      if(slider == 4){
+        setSlider(0)
+      }
+    }, 2000)
+    return()=>{
+      clearInterval(timer)
+    }
+  },[slider])
+
+  const rightData = [
+    {
+      per : "80%",
+      title1 : "Response Rate",
+      title2 : "as compared to standard surveys loream ipsum dolor.",
+      title3 : "Calculate your ROI -→"
+    },
+    {
+      per : "15x",
+      title1 : "ROI",
+      title2 : "more quickly, with more engagement and less attrition.",
+      title3 : "Calculate your ROI -→"
+    }
+  ]
+  
   return (
     <div className="home">
       <div className="card1">
@@ -46,8 +83,14 @@ const Home = () => {
       <div className="card4">
         <div>
           <div>
-            <div><span style={{fontWeight:"bolder"}}>100+</span> enterprices across.</div>
-            <div><span style={{fontWeight:"bolder"}}>100+</span> countries trust us.</div>
+            <div>
+              <span style={{ fontWeight: "bolder" }}>100+</span> enterprices
+              across.
+            </div>
+            <div>
+              <span style={{ fontWeight: "bolder" }}>100+</span> countries trust
+              us.
+            </div>
           </div>
         </div>
         <div>
@@ -56,11 +99,103 @@ const Home = () => {
               400,000+ employees worldwide open up to AppUp without hesitation
               and share invaluable feedback.
             </div>
-            <div style={{color:"blue"}}>View All Customers →</div>
+            <div style={{ color: "blue", cursor: "pointer" }}>
+              View All Customers -→
+            </div>
           </div>
         </div>
       </div>
-      <div className="card5"></div>
+      <div className="card5">
+        <div>
+          <div>
+            <div style={{ fontSize: "30px" }}>AppUp understands all</div>
+            <div style={{ fontSize: "10px", paddingTop: "2%", color: "gray" }}>
+              AppUp not only speaksall these languages, she sounds as natural
+              and conversational as a human being. so employees can express
+              themselves openly.
+            </div>
+            <div
+              style={{ fontSize: "14px", paddingTop: "2%", cursor: "pointer" }}
+            >
+              View Supported Languages -→
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src={flag} alt="" />
+        </div>
+      </div>
+      <div className="card6">
+        <div style={{ width: "50%", height: "40vh" }}>
+          <div>
+            <img src={comma} alt="" style={{ marginTop: "3%" }} />
+          </div>
+          <div
+            style={{
+              fontFamily: "Manrope",
+              paddingLeft: "10%",
+              paddingTop: "2%",
+            }}
+          >
+            90% of employees who Appup connects [with] respond, which I thought
+            was a great win. People are comfortable talking to AppUp; they are
+            comfortable sharing their thoughts and what they are going through.
+          </div>
+          <div
+            style={{
+              width: "50%",
+              fontSize: "18px",
+              color: "#2872E0",
+              fontWeight: "bold",
+              paddingLeft: "10%",
+              paddingTop: "2%",
+            }}
+          >
+            Rohit Sandal CHRO, Mediacom
+          </div>
+        </div>
+        <div 
+        style={{
+          width:"50%", 
+          // border:"1px solid red", 
+          display:"flex",
+          alignItems:"center",
+          justifyContent:"center"
+          }}>
+          <img
+          style={{
+            width:"60%",
+            height:"35vh",
+            display:"flex",
+            alignItems:"center",
+            justifyContent:"center",
+            borderRadius:"50%"
+          }}
+          src={banner[slider].image} alt="" />
+        </div>
+      </div>
+
+      <div className="card7">
+        <div>
+          <div className="what">What AppUp can bring you —</div>
+          <div className="loream">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In volutpat sollicitudin odio, elementum interdum magna sagittis accumsan. Pellentesque laoreet purus vel convallis ultricies. In orci felis, posuere at interdum et, cursus nec metus.</div>
+        </div>
+        <div>
+          {
+            rightData.map((e)=>{
+              return(
+                <div className="highlight">
+                  <div>{e.per}</div>
+                  <div>{e.title1}</div>
+                  <div>{e.title2}</div>
+                  <div>{e.title3}</div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      <div className="card8"></div>
     </div>
   );
 };
